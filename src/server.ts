d1727a8303651d43709fffc723d7df2c1e7767e7
matheus369k <gomes.craft10@gmail.com>
2025-06-20 17:29:28 -0300

    feat: register routes create cors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,32 @@
-import Fastify from 'fastify';
+import { fastify } from 'fastify';
+import cors from '@fastify/cors';
 import { env } from './env';
-import { mongoConnectionDatabase } from './mongo/connection';
+import { mongoConnectionDatabase } from './config/database';
+import {
+	serializerCompiler,
+	validatorCompiler,
+} from 'fastify-type-provider-zod';
+import * as productsRoutes from './route/product';
 
-const fastify = Fastify({
+const app = fastify({
 	logger: true,
 });
 
-fastify.listen(
+app.register(cors, {
+	origin: env.FRONT_END_URL,
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(productsRoutes.routeAddProduct);
+app.register(productsRoutes.routeGetProducts);
+app.register(productsRoutes.routeDeleteProduct);
+
+app.listen(
 	{
 		port: env.PORT,
+		host: env.HOST,
 	},
 	(err, address) => {
 		if (err) {
