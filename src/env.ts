add6b39861b83c888ab2f642ad0fb40b1a520cfe
matheus369k <gomes.craft10@gmail.com>
2025-06-20 17:26:39 -0300

    feat: create variables host and front_end_url

--- src/env.ts
+++ src/env.ts
@@ -2,9 +2,11 @@ import { z } from 'zod';
 
 const envSchema = z.object({
 	PORT: z.coerce.number().optional().default(3000),
+	HOST: z.string().optional().default('0.0.0.0'),
 	DATABASE_URL: z.string().url(),
 	MONGO_DATABASE_NAME: z.string().min(3),
 	MONGO_DATABASE_PASSWORD: z.string().min(3),
+	FRONT_END_URL: z.string().url().optional().default('*'),
 });
 
 export const env = envSchema.parse(process.env);
