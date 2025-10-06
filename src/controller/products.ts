bb2cd4d1214c000c9b7c27acd06d679ebfc753e8
matheus369k <gomes.craft10@gmail.com>
2025-10-06 19:03:10 -0300

    refactor(controller/product): switch function to class

--- src/controller/products.ts
+++ src/controller/products.ts
@@ -1,16 +1,16 @@
+import { mongoDb } from '@/db/mongo/mongo';
 import { ClientError } from 'src/error/client-error';
-import { MongoDatabase } from 'src/model/product';
 
-type SlideAddProductType = {
+type SlideCreateProductType = {
 	slide1: string;
 	slide2: string;
 	slide3: string;
 };
 
-type AddProductProps = {
+type CreateProductProps = {
 	model: string;
 	img: string;
-	slide: SlideAddProductType;
+	slide: SlideCreateProductType;
 	screen: string;
 	processor: string;
 	memory: string;
@@ -20,73 +20,79 @@ type AddProductProps = {
 	category: string;
 };
 
-export const addProduct = async (props: AddProductProps) => {
-	const { _id: productId } = await MongoDatabase.Products.insertOne({
-		category: props.category,
-		img: props.img,
-		memory: props.memory,
-		model: props.model,
-		price: props.price,
-		processor: props.processor,
-		screen: props.screen,
-		slide: props.slide,
-		battery: props.battery,
-		placeVideo: props.placeVideo,
-	});
-	if (!productId) {
-		throw new ClientError('Error to must add one new product...');
-	}
-	return {
-		productId,
-	};
-};
-
-export const getAllProducts = async () => {
-	const products = await MongoDatabase.Products.find().limit(24);
-	if (!products || products.length === 0) {
-		throw new ClientError('Not found product...');
-	}
-	const separationProductsForCategory = products.reduce((acc, curr) => {
-		if (acc[curr.category]) {
-			acc[curr.category].push(curr);
-		} else {
-			return {
-				[curr.category]: [
-					curr,
-				],
-				...(acc || []),
-			};
+export class ProductsControllers {
+	async create(props: CreateProductProps) {
+		const { Products } = await mongoDb();
+		const { _id: productId } = await Products.insertOne({
+			category: props.category,
+			img: props.img,
+			memory: props.memory,
+			model: props.model,
+			price: props.price,
+			processor: props.processor,
+			screen: props.screen,
+			slide: props.slide,
+			battery: props.battery,
+			placeVideo: props.placeVideo,
+		});
+		if (!productId) {
+			throw new ClientError('Error to must add one new product...');
 		}
-		return acc;
-	}, {});
-	if (!separationProductsForCategory) {
-		return null;
+		return {
+			productId,
+		};
 	}
-	return {
-		...separationProductsForCategory,
-	};
-};
 
-export const getCategoryOfProducts = async (category: string) => {
-	const products = await MongoDatabase.Products.where({
-		category: category,
-	}).limit(24);
-	if (!products || products.length === 0) {
-		throw new ClientError('Not found product...');
+	async getAll() {
+		const db = await mongoDb();
+		const products = await db.Products.find().limit(24);
+		if (!products || products.length === 0) {
+			throw new ClientError('Not found product...');
+		}
+		const separationProductsForCategory = products.reduce((acc, curr) => {
+			if (acc[curr.category]) {
+				acc[curr.category].push(curr);
+			} else {
+				return {
+					[curr.category]: [
+						curr,
+					],
+					...(acc || []),
+				};
+			}
+			return acc;
+		}, {});
+		if (!separationProductsForCategory) {
+			return null;
+		}
+		return {
+			...separationProductsForCategory,
+		};
 	}
-	return {
-		products,
-	};
-};
 
-export const deleteProduct = async (id: string) => {
-	const product = await MongoDatabase.Products.findByIdAndDelete({
-		_id: id,
-	});
-	if (!product) {
-		throw new ClientError('Error to must deleted product...');
+	async getForCategory({ category }: Pick<CreateProductProps, 'category'>) {
+		const { Products } = await mongoDb();
+		const products = await Products.where({
+			category,
+		}).limit(24);
+		if (!products || products.length === 0) {
+			throw new ClientError('Not found product...');
+		}
+		return {
+			products,
+		};
 	}
-	return {
-		productId: product._id,
-	};
-};
+
+	async delete(_id: string) {
+		const { Products } = await mongoDb();
+		const product = await Products.findByIdAndDelete({
+			_id,
+		});
+		if (!product) {
+			throw new ClientError('Error to must deleted product...');
+		}
+		return {
+			productId: product._id,
+		};
+	}
+}
