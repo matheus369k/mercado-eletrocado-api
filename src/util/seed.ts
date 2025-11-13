7312e0ea63ecb46e950920d601cb58a700d0a2a7
matheus369k <gomes.craft10@gmail.com>
2025-11-13 18:03:16 -0300

    refactor(seed): replace product images urls

--- src/util/seed.ts
+++ src/util/seed.ts
@@ -8,14 +8,14 @@ const seed = async () => {
 		await Products.insertMany([
 			{
 				model: 'Notebook Samsung Galaxy Book 3 360',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-1-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1pfmWy0ekTQzl-XQZWuYcKLS8NdY3zNVP&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-1-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1pfmWy0ekTQzl-XQZWuYcKLS8NdY3zNVP&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-1-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1evuvxMOvD8LyYeJ1Jmkdm1TnaGF6GdlS&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-1-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1C7d44PnGmQ9kXle2Sk9hb8rfQa-tOXnI&sz=w549',
 				},
 				screen: 'Tela de 14 polegadas',
 				processor: 'Intel Core i5-1135G7',
@@ -26,14 +26,14 @@ const seed = async () => {
 			},
 			{
 				model: 'Lenovo V130-14IKB 81HQ',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-4-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1Pc_Dlxody5Pi_D8u38LXbai4MHeRcynT&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-4-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1Pc_Dlxody5Pi_D8u38LXbai4MHeRcynT&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-4-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1AmG3BW21umUVyhFVqcNLu9fLO1MnlwGc&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-4-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1PHA6HTcw2SGskv49yZZPryzVKAeh8HKX&sz=w549',
 				},
 				screen: ' Tela de 14 polegadas',
 				processor: 'Intel Core i5-1135G7',
@@ -45,14 +45,14 @@ const seed = async () => {
 			},
 			{
 				model: 'Notebook ACER Aspire 5 A515 15.6',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-5-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1nmDfRq36KWvZxAECKgTPdw9dDlwVrg2A&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-5-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1nmDfRq36KWvZxAECKgTPdw9dDlwVrg2A&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-5-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1PcbAaCulFc_qn06oAzIOQZ8KkXqXRQn2&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-5-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1Nh4wSnYlkNabtOcwD_2iDtIOINkdRan9&sz=w549',
 				},
 				screen: 'Tela de 14 polegadas',
 				processor: 'Intel Core i7-1165G7',
@@ -64,14 +64,14 @@ const seed = async () => {
 			},
 			{
 				model: 'Acer Aspire 3 A315-24P-R6SK Notebook',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-6-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1Lwbx6IReLlhjqe5UTUsXxSRllvoAXteM&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-6-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1Lwbx6IReLlhjqe5UTUsXxSRllvoAXteM&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-6-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1xwH3_cAhbYF1ravEhNRrFJ-107X5pgR8&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-6-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1kg-_ZyM7SVeiwXY_BdMhQ-lqU6ai5LIe&sz=w549',
 				},
 				screen: 'Tela de 15,6 polegadas',
 				processor: 'Intel Core i7-1165G7',
@@ -83,14 +83,14 @@ const seed = async () => {
 			},
 			{
 				model: 'Asus VivoBook S501UF-EJ282T Gris',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-11-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1USSThTgAL0-Wlrlt10AldjP11mVJgqqh&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-11-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1USSThTgAL0-Wlrlt10AldjP11mVJgqqh&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-11-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1vsNZqBnFXU7HZEN34a-c28nnSi0LQQk3&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-11-slide-3.png',
+						'https://drive.google.com/thumbnail?id=134wX3J3XXYqsi_Fe-UnsGAXGvlbgxMLx&sz=w549',
 				},
 				screen: 'Tela de 15,6 polegadas',
 				processor: 'Intel Core i5-11300H',
@@ -102,14 +102,14 @@ const seed = async () => {
 			},
 			{
 				model: '2023 Latest Android 11.0 Tablet',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-2-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1KhkkS0F3_ftQWgfbvfo_J34PSE-cQaZw&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-2-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1KhkkS0F3_ftQWgfbvfo_J34PSE-cQaZw&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-2-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1b1KNaaaT-Ryz5mYk__RTDjZCg5ElyuKa&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-2-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1sgPCj2vhC03Sx54LpASlr8MZO3lh85r4&sz=w549',
 				},
 				screen: 'Tela de 11 polegadas',
 				processor: 'M2',
@@ -121,14 +121,14 @@ const seed = async () => {
 			},
 			{
 				model: 'ZONKO Tablet 10.1 Inch Android Tablet',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-8-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=19B-x1tzipgqgOaGDEQ9eEMo4oyK-pvU7&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-8-slide-1.png',
+						'https://drive.google.com/thumbnail?id=19B-x1tzipgqgOaGDEQ9eEMo4oyK-pvU7&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-8-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1NKwYdr0JOqRTLPmL3o2XMdD7WdcvqvNz&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-8-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1QXhRi4yu2wODcrRRIwxnviKU2JdhPPGm&sz=w549',
 				},
 				screen: 'Tela de 12,4 polegadas',
 				processor: 'Qualcomm Snapdragon 888',
@@ -140,14 +140,14 @@ const seed = async () => {
 			},
 			{
 				model: 'Android Tablet Pritom 10 inch Android 10 Tablet',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-9-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1n41Y14VhZ0tRclUx2wgSvs0uAUTXsAlw&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-9-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1n41Y14VhZ0tRclUx2wgSvs0uAUTXsAlw&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-9-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1Z9HdV4QZXuL9TYT57em60gwprh8bsCCZ&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-9-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1xrUSyqxCqFMsFFsfihO4AFrRqRpy6T_R&sz=w549',
 				},
 				screen: 'Tela de 10,9 polegadas',
 				processor: 'A15 Bionic',
@@ -159,14 +159,14 @@ const seed = async () => {
 			},
 			{
 				model: 'G-Tab S30 10.1-Inch',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-10-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=180-OluhYT1b_8mgLJI384x0aX_u9DiC-&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-10-slide-1.png',
+						'https://drive.google.com/thumbnail?id=180-OluhYT1b_8mgLJI384x0aX_u9DiC-&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-10-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1M1Lax31kPaha0zoc5DHHwhhFaXWvm5aL&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-10-slide-3.png',
+						'https://drive.google.com/thumbnail?id=18Rwu_G97c9ZtHIU3kOIkE2TkxMxz5zLQ&sz=w549',
 				},
 				screen: 'Tela de 13 polegadas',
 				processor: 'Qualcomm Snapdragon 7c Gen 2',
@@ -178,14 +178,14 @@ const seed = async () => {
 			},
 			{
 				model: 'Test Sony Xperia Tablet Z',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-15-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1A2CaZsbx_ffWDCgDAzpL_iaD4ZWDFAfb&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-15-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1A2CaZsbx_ffWDCgDAzpL_iaD4ZWDFAfb&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-15-slide-2.png',
+						'https://drive.google.com/thumbnail?id=16X5eWjBf5BMYWtp6hjMDbTU_FAVbIaA5&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-15-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1ccSbP85AVXl6SD_WW1l-I3L-bx4K0J05&sz=w549',
 				},
 				screen: 'Tela de 13 polegadas',
 				processor: 'Intel Core i7-1185G7',
@@ -197,14 +197,14 @@ const seed = async () => {
 			},
 			{
 				model: 'Smartphone Samsung Galaxy S20',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-3-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1I_UeL1Ecft5HNWJgZXwmex8RyJE2xG3O&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-3-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1I_UeL1Ecft5HNWJgZXwmex8RyJE2xG3O&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-3-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1Us2j9Lfm499SKZG4Za59t9ILxXBGf1NR&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-3-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1qi5kGX4T6rDntZ6SVFfzlqZIadAzKloZ&sz=w549',
 				},
 				screen:
 					'6,8 polegadas, Dynamic AMOLED 2X, 1440 x 3200 pixels, taxa de atualização de 120 Hz',
@@ -219,14 +219,14 @@ const seed = async () => {
 			},
 			{
 				model: 'Smartphone Motorola Moto Z Play Xt1635',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-7-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=18pGcxbLF-4W4JmG1RG5k7-GjXaoj8uZP&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-7-slide-1.png',
+						'https://drive.google.com/thumbnail?id=18pGcxbLF-4W4JmG1RG5k7-GjXaoj8uZP&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-7-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1pht6sOokJFopUJsfwoYyWPsrt4me6nPa&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-7-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1zqtsCrqPb6VrLzOdkLo0PUMi_ZHHI7Q7&sz=w549',
 				},
 				screen: 'P-OLED, 6,5 polegadas, 2400×1080 pixels e 120 Hz',
 				processor: 'Qualcomm Snapdragon 750G 5G (8 nm)',
@@ -238,14 +238,14 @@ const seed = async () => {
 			},
 			{
 				model: 'Smartphone Samsung Galaxy S20 FE 6.5',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-12-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1o7qbELSIT8jyRjTHEiw1thXKpFdf8yo2&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-12-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1o7qbELSIT8jyRjTHEiw1thXKpFdf8yo2&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-12-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1iD4mcsGu1rhSONr6en-GWzMAxxuzUXKV&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-12-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1UeTVOW-GQkHF35Or0pjn5OP6GgpRy82K&sz=w549',
 				},
 				screen:
 					'6,8 polegadas, Dynamic AMOLED 2X, 1440 x 3200 pixels, taxa de atualização de 120 Hz',
@@ -259,14 +259,14 @@ const seed = async () => {
 			},
 			{
 				model: 'Celular Iphone 11',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-13-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1a5bk0fd_R5STE5PrDM1MTQRqxazJhAcH&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-13-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1a5bk0fd_R5STE5PrDM1MTQRqxazJhAcH&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-13-slide-2.png',
+						'https://drive.google.com/thumbnail?id=1XQB7Tgp1E7yzVvaqsplLHOLaREXLdeoa&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-13-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1rYHd-4DgUbb18tqmcmBBNvHOcpC0uiyu&sz=w549',
 				},
 				screen:
 					'6,1 polegadas, Retina XDR OLED, 1170 x 2532 pixels, taxa de atualização de 60 Hz',
@@ -280,14 +280,14 @@ const seed = async () => {
 			},
 			{
 				model: 'Smartphone Motorola One XT1941',
-				img: 'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-14-slide-1.png',
+				img: 'https://drive.google.com/thumbnail?id=1TBt6U4I5ctGTaUbx2m-7eyJdhL-0AuZp&sz=w549',
 				slide: {
 					slide1:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-14-slide-1.png',
+						'https://drive.google.com/thumbnail?id=1TBt6U4I5ctGTaUbx2m-7eyJdhL-0AuZp&sz=w549',
 					slide2:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-14-slide-2.png',
+						'https://drive.google.com/thumbnail?id=169Pc1an5GjG7X2d2hA6C9Hc5lgWZwNMj&sz=w549',
 					slide3:
-						'https://raw.githubusercontent.com/matheus369k/matheus369k.github.io/refs/heads/main/eletrocado-images/image-14-slide-3.png',
+						'https://drive.google.com/thumbnail?id=1qOm-V4bDxPVC03-WvJ-C4lPQ0skMGJWG&sz=w549',
 				},
 				screen: 'P-OLED, 6,5 polegadas, 2400×1080 pixels e 120 Hz',
 				processor: 'Qualcomm Snapdragon 750G 5G (8 nm)',
